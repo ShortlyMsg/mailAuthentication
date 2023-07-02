0eb9eca42293919e88a0432f568e79d263907385
M.Said Gürbüz <42msg42@gmail.com>
2023-07-02 15:55:24 +0300

    Security Update* OTP generator is changed with SecureRandom.

--- src/main/java/com/shortlymsg/mailauthentication/service/OneTimePasswordService.java
+++ src/main/java/com/shortlymsg/mailauthentication/service/OneTimePasswordService.java
@@ -2,7 +2,7 @@ package com.shortlymsg.mailauthentication.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 @Service
 public class OneTimePasswordService {
@@ -10,6 +10,6 @@ public class OneTimePasswordService {
     public String generateOneTimePassword() {
 
         return String.valueOf(100000
-                + new Random().nextInt(900000));
+                + new SecureRandom().nextInt(900000));
     }
 }
